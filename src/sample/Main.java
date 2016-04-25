27d73b042e23477fdb140ace349e7de13ebb022e
haakohu <Haak-hu@hotmail.com>
2016-04-25 16:25:12 +0200

    WOOOHOOO

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,37 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
-    private static Stage primaryStage;
+    static Stage primaryStage;
+    static boolean hasCompetition = false;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Main.primaryStage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("dashboard-noActive.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 500, 800));
         primaryStage.show();
     }
+
+
+    @FXML
+    protected static void changeToScene(String scene){
+        try{
+            Parent root = FXMLLoader.load(Main.class.getResource(scene));
+            Main.primaryStage.setScene(new Scene(root));
+        }catch (IOException e){
+        }
+    }
+
 
 
     public static void main(String[] args) {
