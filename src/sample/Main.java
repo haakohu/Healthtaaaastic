6c8df76d11d481a901ddd1c231e451ca14e13994
haakohu <Haak-hu@hotmail.com>
2016-04-25 12:19:20 +0200

    jaja

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,8 +8,10 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Stage primaryStage;
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Main.primaryStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
